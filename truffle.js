a07bfb259389a12cd37b23bdbbb2d217005c122e
AppyFizz <21978236+AppyFizz@users.noreply.github.com>
2018-12-12 12:16:15 -0800

    m2

--- truffle.js
+++ truffle.js
@@ -12,29 +12,41 @@
  *   },
  */
 
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+};
+
+// var DefaultBuilder = require("truffle-default-builder");
+
+// module.exports = {
+//   build: new DefaultBuilder({
+//     "index.html": "index.html",
+//     "app.js": [
+//       "javascripts/app.js"
+//     ],
+//     "app.css": [
+//       "stylesheets/app.css"
+//     ],
+//     "images/": "images/"
+//   }),
+//   networks: {
+//     development: {
+//       host: "localhost",
+//       port: 8545,
+//       network_id: "*" // Match any network id
+//     }
+//   }
+// };
+
 // module.exports = {
 //   // See <http://truffleframework.com/docs/advanced/configuration>
 //   // to customize your Truffle configuration!
+//   networks: {
+//     "development": {
+//       network_id: 2,
+//       host: "localhost",
+//       port: 9545
+//     },
+//   }
 // };
-
-var DefaultBuilder = require("truffle-default-builder");
-
-module.exports = {
-  build: new DefaultBuilder({
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  }),
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
-};
